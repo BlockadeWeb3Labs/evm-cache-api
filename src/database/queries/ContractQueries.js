e41b7b805a3875dbc3a6718e66a3d1672b9421f2
Cybourgeoisie <rbenheidorn@gmail.com>
2020-07-09 15:54:10 -0400

    Adding stats, takes a few seconds to run

--- src/database/queries/ContractQueries.js
+++ src/database/queries/ContractQueries.js
@@ -18,6 +18,41 @@ class ContractQueries {
 		}
 	}
 
+	static getStats() {
+		return {
+			text: `
+				SELECT
+					sq.contract_address,
+					COALESCE(cm.name, cm.custom_name, NULL) AS name,
+					cm.symbol,
+					COUNT(sq.owner) AS num_holders,
+					SUM(sq.amount) AS total,
+					ROUND(AVG(sq.amount), 2) AS mean,
+					PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY sq.amount) AS pct_50th,
+					PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY sq.amount) AS pct_75th,
+					PERCENTILE_CONT(0.90) WITHIN GROUP (ORDER BY sq.amount) AS pct_90th,
+					PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY sq.amount) AS pct_95th
+				FROM (
+						SELECT contract_address, owner, SUM(value) AS amount
+						FROM asset_owner
+						WHERE value > 0
+						GROUP BY contract_address, owner
+					) AS sq,
+					contract_meta cm
+				WHERE
+					sq.amount > 0 AND
+					cm.address = sq.contract_address
+				GROUP BY
+					sq.contract_address,
+					cm.name,
+					cm.custom_name,
+					cm.symbol
+				ORDER BY
+					name ASC;
+			`
+		}
+	}
+
 	static getTokenHolders(
 		contract
 	) {

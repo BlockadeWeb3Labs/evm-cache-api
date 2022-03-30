8afa74503b897b2d51734c8a8403d67dc09de4bc
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-03-30 18:09:26 -0300

    fix: limit and offset usage on the query

--- src/routes/address/getTokensForContract.js
+++ src/routes/address/getTokensForContract.js
@@ -8,46 +8,35 @@ function getTokensForContract(req,res) {
 	let params = getParams(req);
 
 	// Get the defaults
-	let address  = params.address;
-	let contract = params.contract;
-	let limit    = Math.min(parseInt(params.limit, 10) || 50, 200);
-	let offset   = parseInt(params.offset, 10) || 0;
-
-	// Sanity checks
-	limit  = limit > 0   ? limit  : 50;
-	offset = offset >= 0 ? offset :  0;
-
+	const {address, contract, limit = 10, offset = 0}  = params;
+	
 	// Make sure that the email doesn't exist already
 	Database.connect((Client) => {
-		Client.query(AddressQueries.getTokensForContract(address, contract, limit, offset), (result) => {
+		let total = 0;
+		Client.query(AddressQueries.getTokensForContractAmount(address, contract)).then(result => total = Number(result.rows[0].count));
+
+		Client.query(AddressQueries.getTokensForContract(address, contract, Number(limit), Number(offset)), (result) => {
 			Client.release();
 
-			let tokens = [];
 			let contract, name, symbol;
-
-			for (let idx = 0; idx < result.rowCount; idx++) {
-				let row = result.rows[idx];
-
+			
+			const tokens = result.rows.map((row) => {
 				contract = byteaBufferToHex(row.contract);
 				name = row.name;
 				symbol = row.symbol;
 
-				if (parseInt(row.amount, 10) <= 0) {
-					continue;
+				return {
+					'id': row.id,
+					'amount': row.amount,
+					'token_uri': row.token_uri,
+					'metadata': row.metadata
 				}
-
-				tokens.push({
-					'id' : row.id,
-					'amount' : row.amount,
-					'token_uri' : row.token_uri,
-					'metadata' : row.metadata
-				});
-			}
+			})
 
 			return response.send(
 				res,
 				response.OK,
-				{ contract, name, symbol, tokens }
+				{ contract, name, symbol, tokens, total }
 			);
 		});
 	});
